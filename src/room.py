dbd6b029617d5655c7720aa59bfe952e0ec98dcc
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-06-09 18:19:28 -0500

    Created room and player classes.

--- src/room.py
+++ src/room.py
@@ -1,2 +1,32 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f"{self.name} : {self.description}"
+
+    def __repr__(self):
+        return f"self.name = {self.name}\n self.description = {self.description}"
+
+
+# bedroom = Room("Bedroom", "A room where I go to sleep")
+
+# coordinate = []
+
+# x = 0
+# while x < 5:
+#     y = 0
+#     while y < 4:
+#         coordinate.append((x, y))
+#         y += 1
+#     coordinate.append((x, y))
+#     x += 1
+
+# # print(coordinates)
+# print(coordinate)
+# print(coordinate[1])
+# print(coordinate[2])
