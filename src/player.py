469d791319cd3178eb475d45b66c2e964c76a19d
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-06-09 20:07:41 -0500

    Added pseudocode for grab method.

--- src/player.py
+++ src/player.py
@@ -1,7 +1,25 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
+from item import Item
+
+
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, inventory):
         self.name = name
         self.current_room = current_room
+        self.inventory = inventory  # [items...]
+
+        # self.grab():
+        # grab something from the room's inventory, place into player inventory
+        # self.drop()
+        # opposite of grab.
+
+    def __str__(self):
+        return f"{self.name}: {self.current_room}"
+
+    def __repr__(self):
+        return f"self.name = {self.name}\n, self.current_room = {self.current_room}"
+
+    # def grab(item):
+    #     self.inventory.append(item)
