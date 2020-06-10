682020671c93ad26a137093d73ddfff906c29ce1
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-06-09 20:07:16 -0500

    Implemented movement, quit function.

--- src/adv.py
+++ src/adv.py
@@ -1,26 +1,26 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance\n",
+                     "North of you, the cave mount beckons", []),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer\n", """Dim light filters in from the south. Dusty
+passages run north and east.""", ["torch"]),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("Grand Overlook\n", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", ["rope"]),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage\n", """The narrow passage bends here from west
+to north. The smell of gold permeates the air.""", []),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room("Treasure Chamber\n", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", []),
 }
-
 
 # Link rooms together
 
@@ -39,6 +39,44 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+
+def create_player():
+    initiate_player = input("What is your name :)").capitalize()
+    return Player(initiate_player, room['outside'], [])
+
+
+def main():
+    print("\n Welcome to the game. \n")
+    player = create_player()
+    print(player)
+    print(f"\n{player.name} is in {player.current_room}")
+
+    playing = True
+    while playing:
+        choice = input(
+            "Choose a direction - 'n', 's', 'e', 'w', or 'quit' to quit: ").lower()
+        try:
+            if(choice == 'n'):
+                player.current_room = player.current_room.n_to
+                print(player.current_room)
+            elif(choice == 's'):
+                player.current_room = player.current_room.s_to
+                print(player.current_room)
+            elif(choice == 'e'):
+                player.current_room = player.current_room.e_to
+                print(player.current_room)
+            elif(choice == 'w'):
+                player.current_room = player.current_room.w_to
+                print(player.current_room)
+
+            elif(choice == 'look'):
+                print(player.current_room.inventory)
+            elif(choice == 'quit'):
+                playing = False
+
+        except Exception:
+            print("That is not a valid command.")
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +87,7 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+
+if __name__ == '__main__':
+    main()
