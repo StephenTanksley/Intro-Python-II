2f393635b9703553d79d9cd0f4cf179331772662
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-06-11 21:00:37 -0500

    Added new class.
    Added a LightSource class. Will use this structure to create custom classes for custom items. Also created an items dict which I'm exporting to adv.py.

--- src/item.py
+++ src/item.py
@@ -1,11 +1,29 @@
 class Item:
-    def __init__(self, name, description, quantity):
+    def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.quantity = quantity
 
     def __str__(self):
-        return f"{self.name}, {self.quantity} : {self.description}"
+        return f"{self.name}: {self.description}"
 
     def __repr__(self):
-        return f"self.name = {self.name}\n, self.quantity = {self.quantity}\n, self.description = {self.description}"
+        return f"self.name = {self.name}, self.description = {self.description}"
+
+
+class LightSource(Item):
+    def __init__(self, name, description, illuminated):
+        self.illuminated = illuminated
+
+    def __str__(self):
+        return f'{self.name} lit? {self.illuminated}'
+
+    def light(self, name):
+        if (self.illuminated == False):
+            self.illuminated = True
+            print(f"You lit the {self.name}")
+
+
+items = {
+    'torch': LightSource(
+        'torch', 'A simple torch waiting to be lit.', False),
+}
